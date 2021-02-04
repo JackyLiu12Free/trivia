7818d62f743351973cfc51f5a8cdfd265c64aeb3
JackyLiu12Free <47015956+JackyLiu12Free@users.noreply.github.com>
2021-02-03 17:57:31 -0800

    Answer props Stuck on putting in multiple answer props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,38 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  var questionNumber = 0;
+  return (
+    <div className="app">
+      Trivia!
+      <Question text={data[questionNumber].question.text} />
+      <NextQuestion />
+    </div>
+  );
+}
+
+// var questionNumber = 0;
+
+function Question({ text }) {
+  return (
+    <div className="Question">
+      {text}
+      <Answer text={data[questionNumber].question.choices[0]} />
+    </div>
+  );
+}
+
+function NextQuestion({ text }) {
+  return (
+    <div className="NextQuestion">
+      {" "}
+      <button>Next Question</button>{" "}
+    </div>
+  );
+}
+
+function Answer({ text }) {
+  return <div className="Answer">{text} </div>;
 }
 
 export default App;
